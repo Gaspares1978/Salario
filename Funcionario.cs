b2fc02592077f8f9584ac0e518c3df24d8a0a6cf
Gaspares1978 <41311144+Gaspares1978@users.noreply.github.com>
2018-12-06 16:46:28 -0200

    Add files via upload

---
+++ Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Teste2
+{
+    class Funcionario
+    {
+       public  String Nome;
+       public  Double salarioBruto;
+       public  Double Desconto;
+         public  double salarioLiquido()
+        {
+           return  salarioBruto - Desconto;
+            
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            salarioBruto = salarioBruto + (salarioBruto * porcentagem / 100.0);
+        }
+
+        public override string ToString()
+        {
+            return Nome 
+                + "="+ "$" 
+                + salarioLiquido().ToString("F2",CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace Teste2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario x = new Funcionario();
+            double porcent;
+
+            Console.Write("Nome: ");
+            x.Nome = Console.ReadLine();
+            Console.Write("Salario bruto: ");
+            x.salarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Desconto: ");
+            x.Desconto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.WriteLine("Dados do funcionário: ");
+            Console.WriteLine(x.Nome);
+            Console.WriteLine(x.salarioBruto);
+            Console.WriteLine();
+            Console.Write("Deseja aumentar o salário em qual porcentagem? ");
+            porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.AumentarSalario(porcent);
+            Console.WriteLine();
+            Console.WriteLine("Dados do funcionário: " + x);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
+       
+    
+
